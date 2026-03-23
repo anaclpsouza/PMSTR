aa1c101e1a7c9300fb32cdde7e02f5f65e2ebbbf
Ana Clara Souza <anaclarap03@gmail.com>
2026-03-23 14:35:17 -0300

    Solução inicial aleatória

--- Main.cpp
+++ Main.cpp
@@ -9,27 +9,91 @@
 #include "Operation.h"
 #include "ObjectiveFunction.h"
 #include <chrono>
+#include <deque>
+#include <random>
 
 using namespace std;
-int m = 0;
-int o = 0;
-int t = 0;
-int c = 0;
 std::ofstream fileSolution;
+int m, o, t, c;
+std::vector<std::vector<Operation>> maquinas;
+std::map<int, double> tempoMaq;
 using namespace std::chrono;
+
+std::vector<Operation> randomizarOp(std::vector<Operation> vetOperacao)
+{
+    std::map<int, std::deque<Operation>> tarefas;
+    for (const auto &op : vetOperacao)
+    {
+        tarefas[op.idOp].push_back(op);
+    }
+
+    std::vector<Operation> operacoesRandomizadas;
+    std::vector<int> tarefasDisponiveis;
+
+    for (auto const &[id, fila] : tarefas)
+    {
+        tarefasDisponiveis.push_back(id);
+    }
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    while (!tarefasDisponiveis.empty())
+    {
+
+        std::uniform_int_distribution<> dis(0, tarefasDisponiveis.size() - 1);
+        int indexSorteado = dis(g);
+        int taskIdSorteada = tarefasDisponiveis[indexSorteado];
+
+        auto &fila = tarefas[taskIdSorteada];
+        operacoesRandomizadas.push_back(fila.front());
+        fila.pop_front();
+
+        if (fila.empty())
+        {
+            tarefasDisponiveis.erase(tarefasDisponiveis.begin() + indexSorteado);
+        }
+    }
+
+    return operacoesRandomizadas;
+}
+
+void atribuirMaquinas(std::vector<Operation> operacoes)
+{
+    for (const auto &op : operacoes)
+    {
+        int melhorMaq = 0;
+        double menorTempo = -1.0;
+
+        for (int j = 0; j < m; j++)
+        {
+
+            double tempoCalculado = tempoMaq[j] + op.releaseTime + op.processingTime;
+
+            if (menorTempo < 0 || tempoCalculado < menorTempo)
+            {
+                menorTempo = tempoCalculado;
+                melhorMaq = j;
+            }
+        }
+
+        maquinas[melhorMaq].push_back(op);
+        tempoMaq[melhorMaq] += (op.releaseTime + op.processingTime);
+    }
+}
 
 int parseHeaderValue(string line)
 {
     replace(line.begin(), line.end(), ',', ' ');
     stringstream ss(line);
-    ss.imbue(locale("C")); // Força ponto decimal
+    ss.imbue(locale("C"));
     string key;
     int value;
     ss >> key >> value;
     return value;
 }
 
-int main(int argsc, char* argv[])
+int main(int argsc, char *argv[])
 {
     ios_base::sync_with_stdio(false);
     cin.imbue(locale("C"));
@@ -48,12 +112,12 @@ int main(int argsc, char* argv[])
     getline(cin, line);
     getline(cin, line);
 
-    std::vector<std::vector<Operation>> maquinas(m);
     std::vector<Operation> vetOperacao;
     int i = 0;
     int operacao_global = 0;
     std::map<int, std::map<int, int>> controleOp;
-   
+    maquinas.resize(m);
+
     fileSolution.open(argv[1]);
 
     while (getline(cin, line) && !line.empty())
@@ -70,13 +134,14 @@ int main(int argsc, char* argv[])
         if (ss)
         {
             Operation op(i, idJob, idOp, toolSetId - 1, toolSetSize, processingTime, dueDate, releaseTime);
-            int maq = operacao_global % m; // round-robin
-            maquinas[maq].push_back(op);
             vetOperacao.push_back(op);
             controleOp[idJob][idOp] = 0;
             i++;
             operacao_global++;
         }
+
+        std::vector<Operation> opsAleatorias = randomizarOp(vetOperacao);
+        atribuirMaquinas(opsAleatorias);
     }
 
     std::map<int, double> tempo_cinal;
